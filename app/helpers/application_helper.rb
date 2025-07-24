be793d31ed772399a4d131c2964920220c4b8dba
Henry Nawrocki <henry@sztanski.com>
2025-07-24 17:07:23 -0500

    Refactor logo rendering to use helper method for consistency in navbar and devise layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def logo(size = "h2")
+    link_to(root_path, class: "navbar-brand") do
+      content_tag(size) do
+        content_tag(:i, "", class: "bi bi-safe-fill me-2") + " SafePass"
+      end
+    end
+  end
 end
